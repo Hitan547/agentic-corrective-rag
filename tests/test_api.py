f1afbbfff37affdf1cacabecdec0cc04dc90bb71
Hitan K <hitank2004@gmail.com>
2026-04-11 15:54:01 +0530

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,12 @@
-from app import app
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from main import app
+from fastapi.testclient import TestClient
+
+client = TestClient(app)
 
 def test_health():
-    client = app.test_client()
-    response = client.get('/health')
+    response = client.get("/")
     assert response.status_code == 200
